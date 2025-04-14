846540d6b004185cfef8227f79180de5a143fcd6
DALIAALISIDDIG <124632675+DALIAALISIDDIG@users.noreply.github.com>
2025-04-14 11:25:51 +0200

    Update app.py

--- deepfake-main/app.py
+++ deepfake-main/app.py
@@ -52,8 +52,8 @@ survey = ss.StreamlitSurvey("Survey Audio Persuasiveness")
 
 st.title("Welcome, Audio Explorer! 🎧‍")
 
-# text1 = "We've got an audio snippet, Your mission? listen to the audio and rate it!"
-# st.write(text1)
+text1 = "We've got an audio snippet, Your mission? listen to the audio and rate it!"
+st.write(text1)
 
 text2 = "**How to Play**: Listen to each audio clip and share your thoughts."
 st.markdown(text2)
