d23ff5ef37d40154cee0ce7a7c1b8fb344e4f2a2
DALIAALISIDDIG <124632675+DALIAALISIDDIG@users.noreply.github.com>
2025-09-16 15:07:52 +0200

    Update Rate_responses.py

--- deepfake-main/pages/Rate_responses.py
+++ deepfake-main/pages/Rate_responses.py
@@ -700,8 +700,8 @@ with ((st.form(key="form_rating", clear_on_submit=True))):
 
 
 
-    if all([q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q15, q16, q17,q18,q19_private, q20_public, q21_report, q22_downrank, q23_watermark]):
-        st.session_state['count'] += 1
+        if all([q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q15, q16, q17, q18, q19_private, q20_public, q21_report, q22_downrank, q23_watermark]):
+            st.session_state['count'] += 1
 
     except SQLAlchemyError as e:
         st.error(f"Database query failed: {e}")
