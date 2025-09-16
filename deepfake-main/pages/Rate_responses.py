f3c5bf93eeb23558ed1cda0c7a9ce58cc77474ba
DALIAALISIDDIG <124632675+DALIAALISIDDIG@users.noreply.github.com>
2025-09-16 15:42:48 +0200

    Update Rate_responses.py

--- deepfake-main/pages/Rate_responses.py
+++ deepfake-main/pages/Rate_responses.py
@@ -353,10 +353,7 @@ with ((st.form(key="form_rating", clear_on_submit=True))):
         # st.markdown(audio_html, unsafe_allow_html=True)
 
         st.markdown('<h4>Please answer the following questions about the audio clip.</h4>', unsafe_allow_html=True)
-        st.markdown('🔊 If the audio isn't playing, refresh the page or try a different browser.')
-
-        st.markdown('<h5>.</h5>', unsafe_allow_html=True)
-
+        st.markdown("🔊 If the audio isn't playing, refresh the page or try a different browser.")
 
         st.divider()  # Add a divider line
         #  st.markdown('<h4>Speech Speed and Pace</h4>', unsafe_allow_html=True)
